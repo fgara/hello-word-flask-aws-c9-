ba5fc6535d9cdcd2ba41db284707d513f4e1d7af
EC2 Default User <ec2-user@ip-172-31-16-186.us-east-2.compute.internal>
2019-02-08 15:24:47 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+host = 'localhost' # '0.0.0.0' #"127.0.0.1" # I tried all of these ip's
+
+if __name__ == '__main__':
+    app.debug = True
+    #port = int(os.environ.get("PORT", 8080)) # I also tried port was 5000
+    # app.run(host=host, port=port)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
+# This is the error I got 52.15.73.96 The connection has timed out
+#The server at 52.15.73.96 is taking too long to respond.
